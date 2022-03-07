37bbdef8bbd0a098c047839477ae9e4c7cbea3a6
b3nzuk3 <hussenito7@gmail.com>
2022-03-07 17:16:52 +0300

    added click button factionality

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var year = dob.slice(0, 4);
 var month = dob.slice(5, 7);
 var day = dob.slice(8, 10);
 
-function callDayOfWeek(day, month, year) {
+function callDayOfWeek() {
   var a = Math.floor((14 - month) / 12);
   var y = year - a;
   var m = month + 12 * a - 2;
@@ -15,4 +15,12 @@ function callDayOfWeek(day, month, year) {
 }
 
 
-var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+// Data reprensenting Days corresponding with Date values
+
+var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+//Akan Names corresponding with Days and Date values
+var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
