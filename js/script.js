ec6a6713cd69ab5f601574f0b09764fc77b57986
b3nzuk3 <hussenito7@gmail.com>
2022-03-07 17:17:33 +0300

    added the names and data value into a single function

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
-//   The function responsible for calculating the date Value..
+//   The function responsible for calculating the date Value.
 
-var dob = document.querySelector("#dateInput").value;
-var year = dob.slice(0, 4);
-var month = dob.slice(5, 7);
-var day = dob.slice(8, 10);
+document.querySelector("button").addEventListener("click", callDayOfWeek);
+
 
 function callDayOfWeek() {
+  // Data reprensenting Days corresponding with Date values
+
+  var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+
+  //Akan Names corresponding with Days and Date values
+  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var dob = document.querySelector("#dateInput").value;
+  var year = parseInt(dob.slice(0, 4));
+  var month = parseInt(dob.slice(5, 7));
+  var day = parseInt(dob.slice(8, 10));
   var a = Math.floor((14 - month) / 12);
   var y = year - a;
   var m = month + 12 * a - 2;
-  var d = (day + y + Math.floor(y / 4) - Math.floor(y / 100) + //
+  var d = (day + y + Math.floor(y / 4) - Math.floor(y / 100) +
     Math.floor(year / 400) + Math.floor((31 * m) / 12)) % 7;
-  return d;
+  alert(d);
 }
-
-
-// Data reprensenting Days corresponding with Date values
-
-var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-
-//Akan Names corresponding with Days and Date values
-var maleNames=["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-
-var femaleNames=["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
