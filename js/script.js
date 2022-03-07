c9d6c9f3555373a18f5b8d985c44e87afddc6b4e
b3nzuk3 <hussenito7@gmail.com>
2022-03-07 15:29:06 +0300

    Added Data reprensenting Days corresponding with Date values and Akan Names corresponding with Days and Date values.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
-
 //   The function responsible for calculating the date Value..
 
 var dob = document.querySelector("#dateInput").value;
-var year = dob.slice(0,4);
-var month = dob.slice(5,7);
-var day = dob.slice(8,10);
+var year = dob.slice(0, 4);
+var month = dob.slice(5, 7);
+var day = dob.slice(8, 10);
 
 function callDayOfWeek(day, month, year) {
-var a = Math.floor((14 - month) / 12);
-var y = year - a;
-var m = month + 12 * a - 2;
- var d = (day + y + Math.floor(y / 4) - Math.floor(y / 100) +//
-Math.floor(year / 400) + Math.floor((31 * m) / 12)) % 7;
- return d;
-  }
+  var a = Math.floor((14 - month) / 12);
+  var y = year - a;
+  var m = month + 12 * a - 2;
+  var d = (day + y + Math.floor(y / 4) - Math.floor(y / 100) + //
+    Math.floor(year / 400) + Math.floor((31 * m) / 12)) % 7;
+  return d;
+}
+
+
+var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
